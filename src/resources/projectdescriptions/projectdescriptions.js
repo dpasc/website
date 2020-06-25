4494067d8ab715f5a0b52954477cced6e3f7b15e
dpasc <damianpascale@outlook.com>
2020-06-25 17:00:26 +1000

    Fixed card columns

---
+++ src/resources/projectdescriptions/projectdescriptions.js
@@ -0,0 +1,38 @@
+const projects =[
+    {
+        "id":0,
+        "title":"Grande Travel",
+        "subtitle":"e-commerce platform for travel services",
+        "type":"project",
+        "img":"https://via.placeholder.com/300.png/09f/fff",
+        "content":'&lt;p&gt;&lt;strong&gt;Our Opportunity:&lt;/strong&gt;&lt;/p&gt;'
+
+    },
+    {
+        "id":1,
+        "title":"PLKN",
+        "subtitle":"Assists vendors communicate with retail outlets",
+        "type":"project",
+        "img":"https://via.placeholder.com/300.png/09f/fff",
+        "content":'&lt;p&gt;&lt;strong&gt;something else:&lt;/strong&gt;&lt;/p&gt;'
+    },
+    {
+        "id":2,
+        "title":"Barren Wuffet",
+        "subtitle":"Mobile application for traders",
+        "type":"project",
+        "img":"https://via.placeholder.com/300.png/09f/fff",
+        "content":'&#x3C;div&#x3E;Foo &#xA9; bar &#x1D306; baz &#x2603; qux &#x3C;/div&#x3E;',
+    },
+    {
+        "id":3,
+        "title":"Groovy Notes",
+        "subtitle":"My first react app :)",
+        "type":"project",
+        "img":"https://via.placeholder.com/300.png/09f/fff",
+        "content":"&#x3C;div&#x3E;Foo &#xA9; bar &#x1D306; baz &#x2603; qux &#x3C;hr/&#x3E;&#x3C;img src=&#x22;https://via.placeholder.com/300.png/09f/fff&#x22; alt=&#x27;&#x27;/&#x3E;&#x3C;/div&#x3E;;",
+    },
+];
+
+
+export default projects;
