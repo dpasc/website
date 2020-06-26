cc057cced8416b7bf61ecef91aee12a581e3f70a
dpasc <damianpascale@outlook.com>
2020-06-26 11:43:55 +1000

    test deployment

--- src/resources/blogpostitems/blogpostitems.js
+++ src/resources/blogpostitems/blogpostitems.js
@@ -1,46 +1,53 @@
 
 const posts =[
         {
-            "id":1,
+            "id":0,
             "title":"How to unbake a cake",
-            "subtitle":"",
+            "subtitle":"yum!",
+            "type":"blog",
             "img":"https://via.placeholder.com/300.png/09f/fff",
-            "content":""
+            "content":'&lt;p&gt;&lt;strong&gt;Our Opportunity:&lt;/strong&gt;&lt;/p&gt;'
+
+        },
+        {
+            "id":1,
+            "title":"Yo-yo tricks",
+            "subtitle":"Try this!!",
+            "type":"blog",
+            "img":"https://via.placeholder.com/300.png/09f/fff",
+            "content":'&lt;p&gt;&lt;strong&gt;something else:&lt;/strong&gt;&lt;/p&gt;'
         },
         {
             "id":2,
-            "title":"Yo-yo tricks",
-            "subtitle":"Try this!!",
+            "title":"Top 10 beaches",
+            "subtitle":"Great for the summer",
+            "type":"blog",
             "img":"https://via.placeholder.com/300.png/09f/fff",
-            "content":"<p> Something</p>"
+            "content":'&#x3C;div&#x3E;Foo &#xA9; bar &#x1D306; baz &#x2603; qux &#x3C;/div&#x3E;',
         },
         {
             "id":3,
-            "title":"Top 10 beaches",
-            "subtitle":"Great for the summer",
+            "title":"How to program",
+            "subtitle":"For dummies",
+            "type":"blog",
             "img":"https://via.placeholder.com/300.png/09f/fff",
-            "content":""
+            "content":"&#x3C;div&#x3E;Foo &#xA9; bar &#x1D306; baz &#x2603; qux &#x3C;hr/&#x3E;&#x3C;img src=&#x22;https://via.placeholder.com/300.png/09f/fff&#x22; alt=&#x27;&#x27;/&#x3E;&#x3C;/div&#x3E;;",
         },
         {
             "id":4,
-            "title":"How to program",
-            "subtitle":"",
+            "title":"Rick Roll",
+            "subtitle":"Never gonna give you up!!",
+            "type":"blog",
             "img":"https://via.placeholder.com/300.png/09f/fff",
             "content":"For dummies"
         },
         {
             "id":5,
-            "title":"Rick Roll",
-            "subtitle":"Never gonna give you up!!",
-            "img":"https://via.placeholder.com/300.png/09f/fff",
-            "content":""
-        },
-        {
-            "id":6,
             "title":"Out of ideas",
             "subtitle":"To fill this place holder",
+            "type":"blog",
             "img":"https://via.placeholder.com/300.png/09f/fff",
-            "content":""
+            "content":"For dummies"
         }
     ];
 
